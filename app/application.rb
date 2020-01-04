0e269348cd6ff5269fd9d9e6a84913c7e0d471dd
github username <github email address>
2020-01-03 16:38:44 -0800

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+   
+      num_1 = Kernel.rand(1..24)
+
+      if num_1 <= 12
+        resp.write "Good Morning"
+      else
+        resp.write "Good Afternoon"
+      end
+   
+      resp.finish
+   
+  end
+end
